aee6a4bcd478169d4329ed471c4c0f03ca7c4286
faketuna <faketuna.st@gmail.com>
2025-03-16 18:48:17 +0900

    chore: add small document in ApiTest

--- NativeVoteAPITest/NativeVoteApiTest.cs
+++ NativeVoteAPITest/NativeVoteApiTest.cs
@@ -12,9 +12,10 @@ public class NativeVoteApiTest: BasePlugin
     public override string ModuleName => "NativeVoteAPI test";
     public override string ModuleVersion => "0.0.1";
 
-
+    // Declare the nativeVoteApi variable like this
     public static INativeVoteApi? nativeVoteApi;
 
+    // This is optional, but useful property.
     private INativeVoteApi GetAPI()
     {
         if (nativeVoteApi == null)
@@ -29,6 +30,7 @@ public class NativeVoteApiTest: BasePlugin
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
+        // When NativeVoteAPI is not loaded, it will throw the exception ->
         try
         {
             GetAPI();
@@ -37,18 +39,21 @@ public class NativeVoteApiTest: BasePlugin
         {
         }
 
+        // -> And plugin should stop loading or implement some feature to block the NativeVoteAPI from using.
         if (nativeVoteApi == null)
         {
             Server.PrintToConsole("NativeVote API is not available.");
             return;
         }
         
+        // To register OnVotePass listener
         nativeVoteApi.OnVotePass += info =>
         {
             Server.PrintToChatAll($"{info!.VoteInfo.voteIdentifier}");
             Server.PrintToChatAll("Vote passed!!!");
         };
         
+        // To register OnVoteFail listener
         nativeVoteApi.OnVoteFail += info =>
         {
             Server.PrintToChatAll($"{info!.VoteInfo.voteIdentifier}");
@@ -69,9 +74,18 @@ public class NativeVoteApiTest: BasePlugin
         if(client == null)
             return;
         
+        // Requires to creating a NativeVoteInfo
         var potentialClients = Utilities.GetPlayers().Where(p => p is { IsBot: false, IsHLTV: false }).ToList();
         var potentialClientsIndex = potentialClients.Select(p => p.Index).ToList();
-        NativeVoteInfo nInfo = new NativeVoteInfo("TEST_VOTE!", "#SFUI_vote_passed_nextlevel_extend" ,"Details STR", potentialClientsIndex, VoteThresholdType.AbsoluteValue, 0.5F, 5.0F);
+        
+        // You can only use builtin SFUI_vote texts in display string.
+        // for instance, you can use these strings:
+        // #SFUI_vote_passed_nextlevel_extend -> can be use the details string (custom string)
+        // #SFUI_Vote_None -> blank vote
+        //
+        // Another arguments information is provided in code document, see NativeVoteInfo.cs
+        //
+        NativeVoteInfo nInfo = new NativeVoteInfo("TEST_VOTE!", "#SFUI_Vote_loadbackup" ,"Details STR あああああああああ", potentialClientsIndex, VoteThresholdType.AbsoluteValue, 0.5F, 5.0F);
 
         nativeVoteApi.InitiateVote(nInfo);
     }
